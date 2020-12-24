e8e62233747e62dcde9ab18960c01b1c43e38677
Renz <bustilloroendel@gmail.com>
2020-12-24 09:13:27 +0800

    added login and qos_state function using selenium

--- qos_settings.py
+++ qos_settings.py
@@ -8,21 +8,48 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 env_path = '.\\venv\.env'
 load_dotenv(dotenv_path=env_path)
 MY_ROUTER_IP = os.getenv('MY_ROUTER_IP')
-
-def __init__():
+_USERNAME = os.getenv('_USERNAME')
+_PASSWORD = os.getenv('_PASSWORD')
+driver = '' # set a value IDE shows undefined variable even if I set it at global
+import time
+def init_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.headless = False
+    global driver
     driver = webdriver.Chrome(executable_path=PATH, options=options)
     driver.get(MY_ROUTER_IP)
     
 
+def login():
+    loginbutton = driver.find_element_by_id('logIn')
+    loginbutton.click()
+
+    username = driver.find_element_by_id('Frm_Username')
+    password = driver.find_element_by_id('Frm_Password')
+    submit = driver.find_element_by_id('LoginId')
+
+    username.clear()
+    username.send_keys(_USERNAME)
+    password.clear()
+    password.send_keys(_PASSWORD)
+    submit.click()
+
+
 def qos_state():
+    time.sleep(1)
+    internet_button = driver.find_element_by_id('mmInternet')
+    internet_button.click()
+
+    time.sleep(1)
+    qos_button = driver.find_element_by_id('smAdminQos')
+    qos_button.click()
+
     # variable for QoS on or off state examples below
     # use isSelected() pr getAttribute()
     # String str = driver.findElement(By.id("26110162")).getAttribute("checked");
     # driver.findElement(By.id("26110162")).isSelected();
-    pass
+
 
 def gui():
     # add main window
@@ -36,4 +63,6 @@ def main():
     start_dearpygui()
 
 
-__init__()
+init_driver()
+login()
+qos_state()
