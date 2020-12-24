6c45d40b21f7754e02f1985c668a03c92fbf9084
Renz <bustilloroendel@gmail.com>
2020-12-24 14:40:59 +0800

    Finished qos state functions and navigation. No more time.sleep!

--- qos_settings.py
+++ qos_settings.py
@@ -2,6 +2,9 @@ from dearpygui.core import *
 from dearpygui.simple import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 from dotenv import load_dotenv
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -11,7 +14,11 @@ MY_ROUTER_IP = os.getenv('MY_ROUTER_IP')
 _USERNAME = os.getenv('_USERNAME')
 _PASSWORD = os.getenv('_PASSWORD')
 driver = '' # set a value IDE shows undefined variable even if I set it at global
-import time
+qos_on_radio = ''
+qos_off_radio = ''
+
+
+
 def init_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -21,9 +28,14 @@ def init_driver():
     driver.get(MY_ROUTER_IP)
     
 
-def login():
-    loginbutton = driver.find_element_by_id('logIn')
-    loginbutton.click()
+def login_and_navigation():
+    try:
+        loginbutton = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, 'logIn'))
+        )
+    finally:
+        loginbutton.click()
+        print('Found Log In Button and clicked')
 
     username = driver.find_element_by_id('Frm_Username')
     password = driver.find_element_by_id('Frm_Password')
@@ -34,21 +46,58 @@ def login():
     password.clear()
     password.send_keys(_PASSWORD)
     submit.click()
+    print('Logged In')
+
+    try:
+        internet_button = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, 'mmInternet'))
+        )
+    finally:
+        internet_button.click()
+        print('Found Internet Button and clicked')
+
+
+    try:
+        qos_button = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, 'smAdminQos'))
+        )
+ 
+    finally:
+        qos_button.click()
+        print('Found QoS Button and clicked')
+
 
 
 def qos_state():
-    time.sleep(1)
-    internet_button = driver.find_element_by_id('mmInternet')
-    internet_button.click()
+    # qos_state returns boolean if selected or not, tuple
+    global qos_on_radio, qos_off_radio
+    
+    qos_on_radio = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'Enable1'))
+    )
+    qos_on_radio_state = qos_on_radio.is_selected()
 
-    time.sleep(1)
-    qos_button = driver.find_element_by_id('smAdminQos')
-    qos_button.click()
+  
+    qos_off_radio = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'Enable0'))
+    )
+    qos_off_radio_state = qos_off_radio.is_selected()
 
-    # variable for QoS on or off state examples below
-    # use isSelected() pr getAttribute()
-    # String str = driver.findElement(By.id("26110162")).getAttribute("checked");
-    # driver.findElement(By.id("26110162")).isSelected();
+    qos_state_tuple = (qos_on_radio_state, qos_off_radio_state)
+    # (False, True): 'Off', (True, False): 'On
+    return qos_state_tuple
+
+
+def click_radio_on():
+    qos_on_radio.click()
+    apply_button = driver.find_element_by_id('Btn_apply_QoSBasicCfg').click()
+    print('QoS On')
+
+
+def click_radio_off():
+    qos_off_radio.click()
+    apply_button = driver.find_element_by_id('Btn_apply_QoSBasicCfg').click()
+    print('QoS Off')
 
 
 def gui():
@@ -64,5 +113,12 @@ def main():
 
 
 init_driver()
-login()
-qos_state()
+login_and_navigation()
+before_qos_state = qos_state()
+print(before_qos_state)
+click_radio_off()
+after_qos_state = qos_state()
+print(after_qos_state)
+
+
+
